3d0ab33203b907c3d5823a0a996602b62622ebd0
Beyondlimits <hammettslash@gmail.com>
2022-09-06 23:25:28 +0800

    add links each social media button

--- js/slideshows.js
+++ js/slideshows.js
@@ -1,5 +1,5 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
 // Next/previous controls
 function plusSlides(n) {
@@ -9,6 +9,7 @@ function plusSlides(n) {
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
+  showSlides(slideIndex);
 }
 
 function showSlides(n) {
@@ -21,9 +22,20 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
+
+  // set automatic slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+  // end
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active-dot", "");
   }
