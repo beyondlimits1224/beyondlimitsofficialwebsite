5cc88642d2d3a2111282b1ad039155e5c86da92c
BeyondLimits <hammettslash@gmail.com>
2023-02-03 23:58:39 +0800

    add database to the socmed

---
+++ content.php
@@ -0,0 +1,42 @@
+<?php require_once "inc/functions.php"; ?>
+<?php include "inc/header.php"; ?>
+		<?php			
+			// check connection
+			if($conn->connect_error){
+				die("Connection failed: " . $conn->connect_error);
+			}
+		?>
+		<header class="maxWidth ma">
+			<section class="socmed ma mt-2 align-right">
+				<ul class="flex-container">
+					
+			<?php
+			 	$sql = "SELECT * FROM socmed";
+				$result = mysqli_query($conn, $sql);
+				if(!$result){
+					die("Database query failed: " . mysqli_error());
+				}
+
+				if(mysqli_num_rows($result) > 0){
+					while($row = mysqli_fetch_assoc($result)){
+					echo "<li>" . "<a href='#'>" . $row["icons"] . "</a>" . "</li>";
+					}
+				}
+				else{
+					echo "0 results";
+				}
+
+			?>
+				</ul>
+			</section>
+		</header>
+		<main>
+
+
+		</main>
+
+<?php include "inc/footer.php"; ?>
+
+<?php
+	mysqli_close($conn);
+?>
--- index.php
+++ index.php
@@ -1,49 +1,5 @@
-<?php 
-	// database connection setup
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-
-	// create connection
-	$conn = new mysqli($servername, $username, $password);
+<?php
+	header("Location: http://localhost/beyondlimits/content.php");
+exit;
 ?>
-<!DOCTYPE html>
-<html lang="en-US">
-	<head>
-		<meta charset="UTF-8"/>
-		<meta name="description" content="BeyondLimits official website"/>
-		<meta name="keywords" content="portfolios, web development, graphic designing, vfx, 3D modeling, motion graphics, illustration"/>
-		<meta name="author" content="BeyondLimits" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="css/template.css" />
-		<title>BeyondLimits</title>
-		<!--[if lt IE 9]>  
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<![endif]-->
-	</head>
-	<body>
-		<header class="maxWidth ma">
-			<section class="socmed ma mt-2 align-right">
-				<ul class="flex-container">
-					<li><a href="#"><i class="fa-brands fa-square-behance fa-2xl"></i></a></li>
-					<li><a href="#"><i class="fa-brands fa-facebook fa-2xl"></i></a></li>
-					<li><a href="#"><i class="fa-brands fa-github fa-2xl"></i></a></li>
-					<li><a href="#"><i class="fa-brands fa-youtube fa-2xl"></i></a></li>
-					<li><a href="#"><i class="fa-brands fa-instagram fa-2xl"></i></a></li>
-				</ul>
-			</section>
-		</header>
-		<main>
-			<?php			
-				// check connection
-				if($conn->connect_error){
-					die("Connection failed: " . $conn->connect_error);
-				}
-				echo "Connected successfully";
-			?>
-
-		</main>
-	</body>
-</html>
 
